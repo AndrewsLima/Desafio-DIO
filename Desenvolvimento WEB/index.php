75cd72366c2941e2f90000da06ae5886b475623f
AndrewsLima <andrewslima.tec49@gmail.com>
2022-05-03 20:23:39 -0300

    Inclusão de 3 arquivos index.php para testar site.

---
+++ Desenvolvimento WEB/index.php
@@ -0,0 +1,17 @@
+<html>
+	
+		<head>
+			<title>MANO TU É?</title>
+		<head>
+		
+		<body>
+		
+			<?php
+				for ( $i = 0 ; $i < 10 ; $i++ ) {
+					print( "Linha número " . $i . "<br />");
+				}
+			?>
+			
+		<body>
+			
+<html>				
---
+++ Desenvolvimento WEB/index3.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>VOCÊ É ESPECIAL!</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>CLICA AQUI PRIMEIRO!</h3>
+  <div>
+    <p>
+    <i><b>Oi!!!</b></i>
+    </p>
+  </div>
+  <h3>AGORA AQUI!!</h3>
+  <div>
+    <p>
+    <i><b>Tudo bem?</b></i>
+    </p>
+  </div>
+  <h3>DEPOIS AQUI!!!</h3>
+  <div>
+    <p>
+    <i><b>Você é incrível!</b></i>
+    </p>
+  </div>
+  <h3>POR FIM AQUI!!!!</h3>
+  <div>
+    <p>
+    <i><b>Nunca se esqueça disso!</b></i>
+    </p>
+  </div>
+</div>
+  
+</body>
+</html>
